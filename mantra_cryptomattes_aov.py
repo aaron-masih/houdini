ce25ba6a8a3f4580ef4ce2f1f25db670efbc66fc
aaron-masih <76210975+aaron-masih@users.noreply.github.com>
2023-02-22 22:28:08 +0000

    Update mantra_cryptomattes_aov.py
    added missing cryptomatte layer

--- mantra_cryptomattes_aov.py
+++ mantra_cryptomattes_aov.py
@@ -3,6 +3,10 @@ import hou
 for mantra in hou.selectedNodes():
 	cryptomatte = mantra.parm("vm_cryptolayers")
 	cryptomatte.set("2")
+	property1 = mantra.parm("vm_cryptolayerprop1")
+	property1.set("materialname")
+	channel1 = mantra.parm("vm_cryptolayername1")
+	channel1.set("CryptoMaterial")
 	property2 = mantra.parm("vm_cryptolayerprop2")
 	property2.set("name")
 	channel2 = mantra.parm("vm_cryptolayername2")
